a64319567456ea67bc17ed215cfa76d9b0170f6c
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-09-15 22:37:59 -0300

    feat(produto) - Permite adicionar o produto na lista de desejos

--- src/Controllers/product.controller.js
+++ src/Controllers/product.controller.js
@@ -57,4 +57,44 @@ async function addCart (req, res) {
 
 }
 
-export { product, addCart }
+async function addWishlist (req, res) {
+
+    const token = req.headers.authorization?.replace('Bearer ', '');
+
+    if(!token) {
+        return res.send(401);
+    }
+
+    try {
+
+        const session = await db.collection('sessions').findOne({
+            token,
+        });
+
+        if (!session) {
+            return res.send(401);
+        }
+
+        const user = await db.collection('users').findOne({
+            _id: session.userId
+        });
+
+        if (!user) {
+            return res.send(401);
+        }
+
+        const { color, size } = req.body;
+
+        await db.collection('wishlist').insertOne({
+            color,
+            size
+        });
+
+        res.sendStatus(201);
+    } catch (error) {
+        return res.send(error.message);
+      }
+
+}
+
+export { product, addCart, addWishlist }
--- src/Routes/productRouter.js
+++ src/Routes/productRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/product', productController.product);
 router.post('/cart', productController.addCart);
+router.post('/wishlist', productController.addWishlist);
 
 export default router;
