6391b3aebb51e54a9b8b7f4ce1ed19231ac383fe
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-19 03:09:37 -0300

    fix(persistência do item) - Ajustes nas rotas para manter o coração, do item, preenchido, mesmo em casos de logout/login

--- src/Controllers/products.controller.js
+++ src/Controllers/products.controller.js
@@ -80,14 +80,22 @@ async function listAllProducts(req, res) {
 }
 
 async function listWishlistProducts(req, res) {
-  const { user } = res.locals;
+  const token = req.headers.authorization?.replace('Bearer ', '');
 
   try {
-    const products = await db
-      .collection('wishlist')
-      .find({ userId: user._id })
-      .toArray();
-    return res.status(200).send(products);
+    const session = await db.collection('sessions').findOne({ token });
+
+    if (session) {
+      const user = await db
+        .collection('users')
+        .findOne({ _id: session.userId });
+      const products = await db
+        .collection('wishlist')
+        .find({ userId: user._id })
+        .toArray();
+      return res.status(200).send(products);
+    }
+    return res.status(200).send([]);
   } catch (error) {
     return res.status(400).send(error.message);
   }
--- src/Routes/productsRouter.js
+++ src/Routes/productsRouter.js
@@ -14,6 +14,6 @@ router.put(
   productsController.updateProductAmount
 );
 router.get('/chart', authToken, productsController.listSelectedProducts);
-router.get('/wishlist', authToken, productsController.listWishlistProducts);
+router.get('/wishlist', productsController.listWishlistProducts);
 
 export default router;
